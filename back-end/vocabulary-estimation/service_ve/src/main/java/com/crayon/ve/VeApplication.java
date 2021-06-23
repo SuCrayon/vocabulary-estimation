bc865a488f83b8198d9b76d281b09db9291b305a
Crayon <614820984@qq.com>
2021-06-23 14:29:22 +0800

    [add] 搭建前端前后台基础框架，后端服务框架

---
+++ back-end/vocabulary-estimation/service_ve/src/main/java/com/crayon/ve/VeApplication.java
@@ -0,0 +1,11 @@
+package com.crayon.ve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VeApplication.class, args);
+    }
+}
