44149c3cc8344bd851d8081bd399161c353828af
Crayon <614820984@qq.com>
2021-06-23 10:38:19 +0800

    [add] 前端目录结构整理，词汇xlsx以及数据库模型

---
+++ docs/sql/ve.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE `ve` CHARACTER SET 'utf8mb4';
+
+use ve;
+
+drop table if exists word_category;
+
+/*==============================================================*/
+/* Table: word_category                                         */
+/*==============================================================*/
+create table word_category
+(
+   id                   varchar(32) not null  comment '单词分类id',
+   level                int not null  comment '词汇等级',
+   description          varchar(512)  comment '描述',
+   is_deleted           int not null default 0  comment '是否已删除，0：未删除，1：已删除',
+   create_time          datetime not null  comment '创建时间',
+   update_time          datetime not null  comment '更新时间',
+   primary key (id)
+);
+
+alter table word_category comment '单词分类表，单词分类分级';
+
+/*==============================================================*/
+/* Index: idx_word_category_level                               */
+/*==============================================================*/
+create index idx_word_category_level on word_category
+(
+   level
+);
+
+
+
+drop table if exists word;
+
+/*==============================================================*/
+/* Table: word                                                  */
+/*==============================================================*/
+create table word
+(
+   id                   varchar(32) not null  comment '单词id',
+   word_category_id     varchar(32) not null  comment '单词分类id',
+   word                 varchar(128) not null  comment '单词',
+   description          varchar(512)  comment '单词描述',
+   is_deleted           int not null default 0  comment '是否已删除，0：未删除，1：已删除',
+   create_time          datetime not null  comment '创建时间',
+   update_time          datetime not null  comment '更新时间',
+   primary key (id)
+);
+
+alter table word comment '单词表';
+
+/*==============================================================*/
+/* Index: idx_word_word_category_id                             */
+/*==============================================================*/
+create index idx_word_word_category_id on word
+(
+   word_category_id
+);
